a1da604cceed49b82c34c9d04f46577d6ddd1117
Patrick Stoeckle <p.stoeckle@gmx.net>
2023-12-03 23:59:07 +0100

    :white_check_mark: day 3 part two tests

--- tests/test_gear_ratios.py
+++ tests/test_gear_ratios.py
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from unittest import TestCase
+from unittest import TestCase, main
 from pathlib import Path
-from advent_of_code.gear_ratios import solve_puzzle
+from advent_of_code.gear_ratios import solve_puzzle, part_two
 
 
 class QuizTest(TestCase):
@@ -16,3 +16,9 @@ class QuizTest(TestCase):
 
     def test_real(self) -> None:
         self.assertEqual(solve_puzzle(Path("tests") / "rsc" / "real.txt"), 533775)
+
+    def test_part_two(self) -> None:
+        self.assertEqual(part_two(Path("tests") / "rsc" / "t.txt"), 467835)
+
+if "__main__" == __name__:
+    main()
