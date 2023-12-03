c49be5b9894fbdf6fe0d07ea4cd919de10365658
Patrick Stoeckle <p.stoeckle@gmx.net>
2023-12-03 21:37:34 +0100

    :white_check_mark: Tests for quiz 3

---
+++ tests/test_gear_ratios.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from pathlib import Path
+from advent_of_code.gear_ratios import solve_puzzle
+
+
+class QuizTest(TestCase):
+    def test_normal(self) -> None:
+        self.assertEqual(solve_puzzle(Path("tests") / "rsc" / "t.txt"), 4361)
+
+    def test_normal_with_number_at_the_end(self) -> None:
+        self.assertEqual(solve_puzzle(Path("tests") / "rsc" / "t_with_end.txt"), 4361)
