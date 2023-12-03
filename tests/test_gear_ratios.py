8d291fbd55922d065d20a3d2ca4f9d5ed182b56b
Patrick Stoeckle <p.stoeckle@gmx.net>
2023-12-03 21:41:18 +0100

    :white_check_mark: Real test

--- tests/test_gear_ratios.py
+++ tests/test_gear_ratios.py
@@ -9,3 +9,6 @@ class QuizTest(TestCase):
 
     def test_normal_with_number_at_the_end(self) -> None:
         self.assertEqual(solve_puzzle(Path("tests") / "rsc" / "t_with_end.txt"), 4361)
+
+    def test_real(self) -> None:
+        self.assertEqual(solve_puzzle(Path("tests") / "rsc" / "real.txt"), 533775)
